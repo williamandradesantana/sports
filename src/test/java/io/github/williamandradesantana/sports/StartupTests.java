ce1ca037b3da12a3639c1dbfcc8faabbb0ce8284
William Santana <williamandrade1058@gmail.com>
2026-08-01 21:15:19 -0300

    first commit

---
+++ src/test/java/io/github/williamandradesantana/sports/StartupTests.java
@@ -0,0 +1,13 @@
+package io.github.williamandradesantana.sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
