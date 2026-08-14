f28ad67fa68c2d356f464826e6b1cbd245815250
William Santana <williamandrade1058@gmail.com>
2026-08-14 14:11:14 -0300

    refactor(test): move TestApiFootballHttpClient to a shared package
    - Deduplicate the third copy of this test double across league/team/match providers
    - Update ApiFootballLeagueProviderTest/ApiFootballTeamProviderTest imports accordingly

--- src/test/java/io/github/williamandradesantana/sports/infrastructure/externalapi/apifootball/shared/TestApiFootballHttpClient.java
+++ src/test/java/io/github/williamandradesantana/sports/infrastructure/externalapi/apifootball/shared/TestApiFootballHttpClient.java
@@ -1,13 +1,13 @@
-package io.github.williamandradesantana.sports.infrastructure.externalapi.apifootball.team;
+package io.github.williamandradesantana.sports.infrastructure.externalapi.apifootball.shared;
 
 import io.github.williamandradesantana.sports.infrastructure.shared.apifootball.ApiFootballHttpClient;
 import io.github.williamandradesantana.sports.infrastructure.shared.apifootball.ApiFootballProperties;
 import org.springframework.web.client.RestClient;
 
 public class TestApiFootballHttpClient extends ApiFootballHttpClient {
-    private final RestClient restClient;
+   private final RestClient restClient;
 
-    TestApiFootballHttpClient(RestClient restClient) {
+    public TestApiFootballHttpClient(RestClient restClient) {
         super(new ApiFootballProperties("http://unused", "unused"));
         this.restClient = restClient;
     }
